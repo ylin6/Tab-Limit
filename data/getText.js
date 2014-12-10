1c17a135f7dd3de71ec0dca9940e6ca9cca08e30
ylin6 <yuchenglin3@gmail.com>
2014-12-10 01:00:26 -0500

    Cleaned Up Code

--- data/getText.js
+++ data/getText.js
@@ -1,7 +1,6 @@
 var textArea = document.getElementById("edit-box");
 textArea.addEventListener('keyup', function onkeyup(event) {
   if (event.keyCode == 13) {
-    // Remove the newline.
     text = textArea.value.replace(/(\r\n|\n|\r)/gm,"");
     self.port.emit("text-entered", text);
     textArea.value = '';
